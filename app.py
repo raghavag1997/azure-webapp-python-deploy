a379aac576f88286673cb2cd8078ecd56c716c34
Raghav Agarwal <53235979+raghavag1997@users.noreply.github.com>
2022-02-22 17:45:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,15 @@
 from flask import Flask,render_template, request
 from flask_mysqldb import MySQL
-import pyodbc
-
-server = 'vnetpoc321.database.windows.net'
-database = 'mydb'
-username = 'admin321'
-password = 'Ragh@db31'
-driver = '{ODBC Driver 17 for SQL Server}'
-
-
-cnxn = pyodbc.connect('DRIVER=' + driver +
-                      ';SERVER=' + server +
-                      ';DATABASE=' + database +
-                      ';UID=' + username +
-                      ';PWD=' + password)
 
 app = Flask(__name__)
 
-# #Here I used Private IP To test the things 
-# app.config['MYSQL_HOST'] = '10.0.1.4'
-# app.config['MYSQL_USER'] = 'admin'
-# app.config['MYSQL_PASSWORD'] = 'admin'
-# app.config['MYSQL_DB'] = 'mydb'
+#Here I used Private IP To test the things 
+app.config['MYSQL_HOST'] = 'vnetpoc321.database.windows.net''
+app.config['MYSQL_USER'] = 'admin321'
+app.config['MYSQL_PASSWORD'] = 'Ragh@db31'
+app.config['MYSQL_DB'] = 'mydb'
  
-# mysql = MySQL(app)
+mysql = MySQL(app)
 
 @app.route("/")
 def index():
@@ -31,19 +17,12 @@ def index():
 
 @app.route("/getdata")
 def getdata():
-    cursor = cnxn.cursor()
-    print('Connection established')
+    #Creating a connection cursor
+    cursor = mysql.connection.cursor()
     cursor.execute("select * from MyUsers;")
-    data=cursor.fetchall()
+    data = cursor.fetchall()
     print(data)
     return str(data)
-
-#     #Creating a connection cursor
-#     cursor = mysql.connection.cursor()
-#     cursor.execute("select * from mydb.MyUsers;")
-#     data = cursor.fetchall()
-#     print(data)
-#     return str(data)
 
 # main driver function
 if __name__ == '__main__':

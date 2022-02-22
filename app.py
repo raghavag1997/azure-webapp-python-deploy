e81373b2b76251576804412be8378a979a4d589b
Raghav Agarwal <53235979+raghavag1997@users.noreply.github.com>
2022-02-22 15:24:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+#Here I used Private IP To test the things 
+app.config['MYSQL_HOST'] = '10.0.1.4'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'admin'
+app.config['MYSQL_DB'] = 'mydb'
+ 
+mysql = MySQL(app)
+
 @app.route("/")
 def index():
-    return "Hello From Github Actions"
- 
+    return "Hello World"
+
+@app.route("/getdata")
+def getdata():
+    #Creating a connection cursor
+    cursor = mysql.connection.cursor()
+    cursor.execute("select * from mydb.MyUsers;")
+    data = cursor.fetchall()
+    print(data)
+    return str(data)
+
+# main driver function
+if __name__ == '__main__':
+  
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
